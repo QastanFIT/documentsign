a74ed74544f902815a9f11c407d6cda538f59dc2
Robin Yzewyn | Qastan <Robin@qastan.be>
2024-01-26 14:57:42 +0100

    add eid

--- next.config.js
+++ next.config.js
@@ -1,24 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     webpack: (config, { isServer }) => {
-        if(isServer){
-            config.externals = {
-                'pkcs11js': 'commonjs pkcs11js'
-            }
-            config.module.rules.push({
-                test: /\.node$/,
-                loader: 'node-loader'
-            });
-            config.target = 'node';
-        }
-        else {
+        // if(isServer){
+        //     config.externals = {
+        //         'pkcs11js': 'commonjs pkcs11js'
+        //     }
+        //     config.module.rules.push({
+        //         test: /\.node$/,
+        //         loader: 'node-loader'
+        //     });
+        //     config.target = 'node';
+        // }
+        // else {
             config.resolve.alias.canvas = false;
             config.resolve.alias.encoding = false;
-        }
+        //}
         
         return config;
     },
-    experimental: { webpackBuildWorker:true },
+    experimental: { 
+        //webpackBuildWorker:true 
+    },
 }
 
 module.exports = nextConfig
